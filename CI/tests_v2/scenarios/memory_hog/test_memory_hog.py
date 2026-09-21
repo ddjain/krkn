01e664727e66c73acf9bcc6426094a3c45f9e60d
ddjain <darjain@redhat.com>
2026-09-22 02:22:35 +0530

    test: make CI v2 scenarios cross-cluster compatible

--- CI/tests_v2/scenarios/memory_hog/test_memory_hog.py
+++ CI/tests_v2/scenarios/memory_hog/test_memory_hog.py
@@ -49,6 +49,7 @@ class TestMemoryHog(BaseScenarioTest):
 
     @pytest.mark.no_workload
     @pytest.mark.order(1)
+    @pytest.mark.kind_only
     def test_memory_hog_success_lifecycle_and_targeting(self):
         """Happy path: a hog pod is created on the node-selector target with the configured memory size, the run succeeds, and the pod is cleaned up."""
         nodes = schedulable_worker_nodes(self.k8s_core)
